01e292b7a951065286f3acf00c8b4c427ce9e993
Júlio Luchiari <julio.luchiari@gmail.com>
2017-07-18 17:35:11 -0300

    Code refactoring

--- routes/routes.rb
+++ routes/routes.rb
@@ -8,71 +8,52 @@ require 'sinatra'
 require 'date'
 
 class MyAppRoutes < Sinatra::Base
-    URI = URI('http://www.calendario.com.br/api/api_feriados.php')
+  URI = URI('http://www.calendario.com.br/api/api_feriados.php')
+  TOKEN = 'dnAudmluaWNpdXMucGVyZWlyYUBnbWFpbC5jb20maGFzaD0xNTMxMTU2MTc='
 
-    before do
-        content_type :json
+  before do
+    content_type :json
+  end
+
+  get '/:year/:state/:city/range/:start_date/:end_date' do
+    query_hash = get_response(params)
+
+    unless query_hash["events"]["error_msg"]
+      start_date = Date.strptime("#{ params[:year] }-#{ params[:start_date] }", "%Y-%m-%d").to_time
+      end_date = Date.strptime("#{ params[:year] }-#{ params[:end_date] }", "%Y-%m-%d").to_time
+
+      return_hash = query_hash["events"]["event"].select { |event|
+        event_date = Date.strptime(event["date"], "%d/%m/%Y").to_time
+        event_date.between?(start_date, end_date)
+      }
+
+      JSON.pretty_generate(return_hash)
+    else
+      JSON.pretty_generate(query_hash)
     end
+  end
 
-    get '/' do
-        get_response()
-    end
+  get '/?:year?/?:state?/?:city?' do
+    JSON.pretty_generate(get_response(params))
+  end
 
-    get '/:year/:state/:city/range/:start_date/:end_date' do
-      query_hash = get_query_hash(year: params[:year], state: params[:state], city: params[:city])
+  def get_response(options = { year: DateTime.now.year, state: '', city: '' })
+    query = build_query(options)
 
-      unless query_hash["events"]["error_msg"]
-        start_date = Date.strptime("#{ params[:year] }-#{ params[:start_date] }", "%Y-%m-%d").to_time
-        end_date = Date.strptime("#{ params[:year] }-#{ params[:end_date] }", "%Y-%m-%d").to_time
+    response = HTTParty.get(URI, :query => query)
+    data = response.parsed_response
 
-        return_hash = query_hash["events"]["event"].select { |event|
-          event_date = Date.strptime(event["date"], "%d/%m/%Y").to_time
-          event_date.between?(start_date, end_date)
-        }
+    Hash.from_xml(data)
+  end
 
-        JSON.pretty_generate(return_hash)
-      else
-        JSON.pretty_generate(query_hash)
-      end
-    end
+  def build_query(options = { year: '', state: '', city: ''})
+    query = {
+      "ano" => options[:year].to_s,
+      "estado" => options[:state].to_s,
+      "cidade" => options[:city].to_s,
+      "token" => TOKEN
+    }
 
-    get '/:year' do
-        get_response(year: params[:year])
-    end
-
-    get '/:year/:state' do
-        get_response(year: params[:year], state: params[:state])
-    end
-
-    get '/:year/:state/:city' do
-      get_response(year: params[:year], state: params[:state], city: params[:city])
-    end
-
-    def get_response(year: DateTime.now.year, state: '', city: '')
-        JSON.pretty_generate(get_query_hash(year: year, state: state, city: city))
-    end
-
-    def get_query_hash(year: DateTime.now.year, state: '', city: '')
-      token = 'dnAudmluaWNpdXMucGVyZWlyYUBnbWFpbC5jb20maGFzaD0xNTMxMTU2MTc='
-
-      query = build_query(year: year, state: state, city: city, token: token)
-
-      response = HTTParty.get(URI, :query => query)
-      data = response.parsed_response
-
-      puts data.encoding
-
-      Hash.from_xml(data)
-    end
-
-    def build_query(year:, state:, city:, token:)
-        query = {
-            "ano" => year.to_s,
-            "estado" => state.to_s,
-            "cidade" => city.to_s,
-            "token" => token.to_s
-        }
-
-        query
-    end
+    query
+  end
 end
