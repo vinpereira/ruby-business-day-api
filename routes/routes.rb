b2107f873eb950cf0e67fa1ae7426a2b70aeec7b
Júlio Luchiari <julio.luchiari@gmail.com>
2017-07-18 16:42:21 -0300

    Added endpoint for date range

--- routes/routes.rb
+++ routes/routes.rb
@@ -5,6 +5,7 @@ require 'date'
 require 'httparty'
 require 'json'
 require 'sinatra'
+require 'date'
 
 class MyAppRoutes < Sinatra::Base
     URI = URI('http://www.calendario.com.br/api/api_feriados.php')
@@ -17,27 +18,52 @@ class MyAppRoutes < Sinatra::Base
         get_response()
     end
 
-    get '/year/:year' do
+    get '/:year/:state/:city/range/:start_date/:end_date' do
+      query_hash = get_query_hash(year: params[:year], state: params[:state], city: params[:city])
+
+      unless query_hash["events"]["error_msg"]
+        start_date = Date.strptime("#{ params[:year] }-#{ params[:start_date] }", "%Y-%m-%d").to_time
+        end_date = Date.strptime("#{ params[:year] }-#{ params[:end_date] }", "%Y-%m-%d").to_time
+
+        return_hash = query_hash["events"]["event"].select { |event|
+          event_date = Date.strptime(event["date"], "%d/%m/%Y").to_time
+          event_date.between?(start_date, end_date)
+        }
+
+        JSON.pretty_generate(return_hash)
+      else
+        JSON.pretty_generate(query_hash)
+      end
+    end
+
+    get '/:year' do
         get_response(year: params[:year])
     end
 
-    get '/state/:state' do
-        get_response(state: params[:state])
+    get '/:year/:state' do
+        get_response(year: params[:year], state: params[:state])
+    end
+
+    get '/:year/:state/:city' do
+      get_response(year: params[:year], state: params[:state], city: params[:city])
     end
 
     def get_response(year: DateTime.now.year, state: '', city: '')
-        token = 'dnAudmluaWNpdXMucGVyZWlyYUBnbWFpbC5jb20maGFzaD0xNTMxMTU2MTc='
-
-        query = build_query(year: year, state: state, city: city, token: token)
-
-        response = HTTParty.get(URI, :query => query)
-        data = response.parsed_response
-
-        puts data.encoding
-
-        JSON.pretty_generate(Hash.from_xml(data))
+        JSON.pretty_generate(get_query_hash(year: year, state: state, city: city))
     end
 
+    def get_query_hash(year: DateTime.now.year, state: '', city: '')
+      token = 'dnAudmluaWNpdXMucGVyZWlyYUBnbWFpbC5jb20maGFzaD0xNTMxMTU2MTc='
+
+      query = build_query(year: year, state: state, city: city, token: token)
+
+      response = HTTParty.get(URI, :query => query)
+      data = response.parsed_response
+
+      puts data.encoding
+
+      Hash.from_xml(data)
+    end
 
     def build_query(year:, state:, city:, token:)
         query = {
@@ -46,7 +72,7 @@ class MyAppRoutes < Sinatra::Base
             "cidade" => city.to_s,
             "token" => token.to_s
         }
-        
+
         query
     end
 end
