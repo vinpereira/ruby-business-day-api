e04ed9068d47c3965c66c89731f4bfebcd3d70cb
Júlio Luchiari <julio.luchiari@gmail.com>
2017-07-19 12:33:19 -0300

    Added endpoint to check holidays

--- routes/routes.rb
+++ routes/routes.rb
@@ -10,9 +10,24 @@ require 'date'
 class MyAppRoutes < Sinatra::Base
   URI = URI('http://www.calendario.com.br/api/api_feriados.php')
   TOKEN = 'dnAudmluaWNpdXMucGVyZWlyYUBnbWFpbC5jb20maGFzaD0xNTMxMTU2MTc='
+  OPTIONAL_DAY_OFF_TYPE = 4
+  CONVENTIONAL_HOLIDAY_TYPE = 9
 
   before do
     content_type :json
+  end
+
+  get '/:state/:city/holiday/:date' do
+    date = Date.strptime("#{ params[:date] }", "%Y-%m-%d").to_time
+    params[:year] = date.year
+
+    query_hash = get_response(params)
+
+    unless query_hash["events"]["error_msg"]
+      JSON.pretty_generate({holiday: holiday?(date: date, holidays: query_hash["events"]["event"]) })
+    else
+      JSON.pretty_generate(query_hash)
+    end
   end
 
   get '/:year/:state/:city/range/:start_date/:end_date' do
@@ -22,10 +37,10 @@ class MyAppRoutes < Sinatra::Base
       start_date = Date.strptime("#{ params[:year] }-#{ params[:start_date] }", "%Y-%m-%d").to_time
       end_date = Date.strptime("#{ params[:year] }-#{ params[:end_date] }", "%Y-%m-%d").to_time
 
-      return_hash = query_hash["events"]["event"].select { |event|
+      return_hash = query_hash["events"]["event"].select do |event|
         event_date = Date.strptime(event["date"], "%d/%m/%Y").to_time
         event_date.between?(start_date, end_date)
-      }
+      end
 
       JSON.pretty_generate(return_hash)
     else
@@ -44,6 +59,16 @@ class MyAppRoutes < Sinatra::Base
     data = response.parsed_response
 
     Hash.from_xml(data)
+  end
+
+  def holiday?(date:, holidays:)
+    return_hash = holidays.select do |event|
+      event_date = Date.strptime(event["date"], "%d/%m/%Y").to_time
+      holiday_type = event["type_code"].to_i
+      event_date == date && holiday_type != OPTIONAL_DAY_OFF_TYPE && holiday_type != CONVENTIONAL_HOLIDAY_TYPE
+    end
+
+    return_hash.length > 0
   end
 
   def build_query(options = { year: '', state: '', city: ''})
