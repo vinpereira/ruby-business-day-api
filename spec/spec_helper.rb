bd380fb6881b38d09f6ab82b5e9899f707cbb36d
Vinícius Pereira <jedipereira@gmail.com>
2017-07-26 18:51:47 -0300

    fix Travis CI problem with mongoid-rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# Gems added
+require 'mongoid-rspec'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -97,4 +101,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  # Using mongoid-rspec gem
+  config.include Mongoid::Matchers
 end
