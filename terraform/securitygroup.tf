d52e03634711207a08e4c1c6e95c88bd1cf08a46
Júlio Luchiari <julio.luchiari@gmail.com>
2017-08-03 00:20:15 -0300

    Update cidr block for http traffic

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "business-days-allow-http" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0", "::/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags {
